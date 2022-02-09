32c91cd88a0b0eda891e864e73e815cf329ff7cb
oeleomar <eleomar.dorneles.18@outlook.com>
2022-02-09 16:16:11 -0400

    Criada a base do projeto, + fatures soon

--- game.js
+++ game.js
@@ -2,6 +2,12 @@ const canvas = document.querySelector('#canvas-game')
 const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
+const scoreEL = document.querySelector('#scoreEL')
+const startGamebtn = document.querySelector('#button-game')
+const modolEL = document.querySelector('#modolEL')
+const final_score = document.querySelector('#final-score')
+console.log(scoreEL)
+
 
 class Player {
     constructor(x, y, radius, color) {
@@ -20,8 +26,20 @@ class Player {
 }
 const x = canvas.width /2
 const y = canvas.height /2
-const player = new Player (x, y, 30, 'blue')
 
+let player = new Player (x, y, 10, 'white')
+let projectiles = []
+let enemies = []
+let particles = []
+let score = 0
+
+function init(){
+    player = new Player (x, y, 10, 'white')
+    projectiles = []
+    enemies = []
+    particles = []
+    score = 0
+}
 
 class Projectile{
     constructor(x, y, radius, color, velocity){
@@ -45,15 +63,15 @@ class Projectile{
 
 }
 
-const projectiles = []
+
 
 window.addEventListener('click', (Event) => {
     const angle = Math.atan2(Event.clientY - canvas.height /2, Event.clientX - canvas.width /2)
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin (angle)
+        x: Math.cos(angle) * 6,
+        y: Math.sin (angle) * 6
     }
-    projectiles.push(new Projectile(canvas.width /2, canvas.height /2, 5, 'red', velocity))
+    projectiles.push(new Projectile(canvas.width /2, canvas.height /2, 4, 'white', velocity))
 })
 
 class Enemy{
@@ -77,7 +95,7 @@ class Enemy{
     }
 
 }
-const enemies = []
+
 
 function spawnEnemies(){
     setInterval(() => {
@@ -93,48 +111,154 @@ function spawnEnemies(){
         }
        
         
-        const color = 'green'
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
         const angle = Math.atan2(canvas.height /2 - y, canvas.width /2 - x)
-        const velocity = {
-            x: Math.cos(angle),
-            y: Math.sin (angle)
+        if (radius < 10){
+            const velocity = {
+                x: Math.cos(angle) *3,
+                y: Math.sin (angle) *3
+            } 
+            enemies.push(new Enemy(x, y, radius, color, velocity))
+        }else if (radius >=10 && radius < 20){
+            const velocity = {
+                x: Math.cos(angle) *2,
+                y: Math.sin (angle) *2
+            } 
+            enemies.push(new Enemy(x, y, radius, color, velocity))
+        }else{
+            const velocity = {
+                x: Math.cos(angle),
+                y: Math.sin (angle)
+            } 
+            enemies.push(new Enemy(x, y, radius, color, velocity))
         }
-
-        enemies.push(new Enemy(x, y, radius, color, velocity))
+        
     }, 1000)
+}
+
+const friction = 0.99
+
+class Particle{
+    constructor(x, y, radius, color, velocity){
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+        this.alpha = 1
+    }
+    draw(){
+        c.save()
+        c.globalAlpha = this.alpha
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+        c.restore()
+    }
+    update(){
+        this.draw()
+        this.velocity.x *= friction
+        this.velocity.y *= friction
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+        this.alpha -= 0.01
+    }
+
 }
 
 let animationId
 
+
 function animate(){
     animationId = requestAnimationFrame(animate)
-    c.clearRect(0 , 0, canvas.width, canvas.height)
+    c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+    c.fillRect(0 , 0, canvas.width, canvas.height)
     player.draw()
-    projectiles.forEach(projectile => {
+
+    particles.forEach((particle, index) =>{
+        particle.update()
+        if (particle.alpha <= 0){
+            particles.splice(index, 1)
+        }
+    })
+
+    projectiles.forEach((projectile, index) => {
         projectile.update()
+
+        if (projectile.x - projectile.radius < 0 || 
+            projectile.x - projectile.radius > canvas.width ||
+            projectile.y + projectile.radius <0 ||
+            projectile.y - projectile.radius > canvas.height){
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0)
+        }
     })
 
     enemies.forEach((enemy, index) => {
         enemy.update()
         const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
         if (dist - enemy.radius - player.radius < 1){
-
+            cancelAnimationFrame(animationId)
+            modolEL.style.display = 'flex'
+            final_score.innerText = score
         }
-
 
         projectiles.forEach((projectile, projectileIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+           
+            //Projeto toca os inimigos
             if (dist - enemy.radius - projectile.radius < 1){
-                setTimeout(() => {
-                    enemies.splice(index, 1)
-                    projectiles.splice(projectileIndex, 1)
-                }, 0) //Flash gone. 
+
+                
+
+                //Explosões
+                for(let i = 0; i < enemy.radius * 2; i++){
+                    particles.push(new Particle(
+                        projectile.x, 
+                        projectile.y, 
+                        Math.random() * 2, 
+                        enemy.color, 
+                        {
+                            x: (Math.random() - 0.5) * (Math.random() * 6),
+                            y: (Math.random() - 0.5) * (Math.random() * 6)
+                        }))
+                }
+
+                if (enemy.radius -10 > 5){
+                    //Score
+                    score +=100
+                    scoreEL.innerText = score
+                    gsap.to(enemy, {
+                        radius: enemy.radius - 10,
+
+                    })
+                    setTimeout(() => {
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0) //Flash gone.
+                } else {
+                    //Score
+                    score +=250,
+                    scoreEL.innerText = score
+                    setTimeout(() => {
+                        enemies.splice(index, 1)
+                        projectiles.splice(projectileIndex, 1)
+                    }, 0) //Flash gone.
+                }               
             }
         })
     })
 }
-animate()
-spawnEnemies()
+
+startGamebtn.addEventListener('click', () =>{
+    init()
+    animate()
+    spawnEnemies()
+    
+    modolEL.style.display = 'none'
+})
+
 
 
 
